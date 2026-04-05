32b4e753287ab3c4e5e315609494a4ffce0b1032
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-04-05 21:21:15 +0530

    first commit

---
+++ Backtracking/subset.java
@@ -0,0 +1,23 @@
+package Backtracking;
+
+public class subset {
+    public static void findsubset(String str, String ans, int i){
+//         base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            }
+            System.out.println(ans);
+            return;
+        }
+//        recursion
+//        yes
+        findsubset(str, ans+str.charAt(i), i+1);
+//        no choice
+        findsubset(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findsubset(str, "", 0);
+    }
+}
