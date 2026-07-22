e7f624da0fb36325e81afabefb28fa41c34cb5eb
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-07-22 20:38:19 +0530

    delete a node

--- Trees/BinarySearchTree/Bst.java
+++ Trees/BinarySearchTree/Bst.java
@@ -51,19 +51,61 @@ public class Bst {
 
     }
 
+    public static Node delete(Node root, int val){
+        if(root.data < val){
+            root.right = delete(root.right, val);
+        }
+        else if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else{
+            // case 1 leaf node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+//            case 2 single child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+            // case 3 both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+
+        }
+        return root;
+    }
+
+    private static Node findInorderSuccessor(Node root) {
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+//        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = {8, 5,3,1, 4,6,10, 11,14};
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
 
         inorder(root);
-        if(search(root, 1) ){
-            System.out.println("Found at "+ root.data);
-        }else{
-            System.out.println("Not found");
-        }
+//        if(search(root, 1) ){
+//            System.out.println("Found at "+ root.data);
+//        }else{
+//            System.out.println("Not found");
+//        }
+
+        System.out.println();
+        root = delete(root, 1);
+        System.out.println();
+        inorder(root);
+
 
     }
 }
