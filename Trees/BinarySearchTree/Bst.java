1163059dd1b54e65224d68fb4c3873bff54d9bc8
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-07-26 01:17:13 +0530

    add validate question

--- Trees/BinarySearchTree/Bst.java
+++ Trees/BinarySearchTree/Bst.java
@@ -1,5 +1,7 @@
 package Trees.BinarySearchTree;
 
+import java.util.ArrayList;
+
 public class Bst {
     static class Node {
 
@@ -103,6 +105,41 @@ public class Bst {
         }
     }
 
+    public static void printPath(ArrayList<Integer> path) {
+            for(int i=0; i<path.size(); i++){
+                System.out.print(path.get(i) + " -> ");
+            }
+        System.out.println("null");
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() -1);
+    }
+
+    public static boolean isValidBST(Node root, Node min, Node max){
+           if(root == null){
+               return true;
+           }
+           if(min != null && root.data <= min.data){
+               return false;
+           }
+            else if(max != null && root.data >= max.data){
+            return false;
+        }
+            return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+
+
     public static void main(String[] args) {
 //        int values[] = { 5, 1, 3, 4, 2, 7 };
         int values[] = {8, 5,3,1, 4,6,10, 11,14};
@@ -123,7 +160,13 @@ public class Bst {
 //        System.out.println();
         inorder(root);
         System.out.println();
-        printInRange(root, 5, 12);
+//        printInRange(root, 5, 12);
+//        printRoot2Leaf(root, new ArrayList<>());
+        if(isValidBST(root, null, null)){
+            System.out.println("valid BST");
+        }else{
+            System.out.println("not valid BST");
+        }
 
 
     }
