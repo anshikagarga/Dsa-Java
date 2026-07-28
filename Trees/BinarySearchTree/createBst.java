c1a893dbab3a4782e07076213a3830c9aa2d5da2
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-07-28 17:11:06 +0530

    add createBst

--- Trees/BinarySearchTree/createBst.java
+++ Trees/BinarySearchTree/createBst.java
@@ -34,9 +34,47 @@ public class createBst {
         return root;
     }
 
+    public static void getInorder(Node root, ArrayList<Integer> inorder){
+        if(root == null){
+            return;
+        }
+        getInorder(root.left,inorder);
+        inorder.add(root.data);
+        getInorder(root.right,inorder);
+    }
+    public static Node createBSt(ArrayList<Integer> inorder, int st, int end){
+        if(st > end){
+            return null;
+        }
+        int mid = (st+end)/2;
+        Node root = new Node(inorder.get(mid));
+        root.left = createBSt(inorder, st, mid-1);
+        root.right = createBSt(inorder, mid+1, end);
+        return root;
+    }
+    public static Node balanceBst(Node root){ //o(n)
+        //inorder seq
+        ArrayList<Integer> inorder = new ArrayList<>();
+        getInorder(root, inorder);
+        // sorted inorder -> balanced bst
+        root = createBSt(inorder,0,inorder.size()-1);
+        return root;
+
+    }
+
     public static void main(String[] args) {
-        int arr[] = {3,5,6,8,10,11,12};
-        Node root = createBST(arr,0,arr.length-1);
-        preOrder(root);
+        Node root = new Node(8);
+        root.left = new Node(6);
+        root.right = new Node(10);
+        root.left.left = new Node(5);
+        root.left.left.left = new Node(3);
+        root.right.right = new Node(11);
+        root.right.right.right = new Node(12);
+//        int arr[] = {3,5,6,8,10,11,12};
+//        Node root = createBST(arr,0,arr.length-1);
+//        preOrder(root);
+       root =  balanceBst(root);
+       preOrder(root);
+
     }
 }
